7a2834928c176336afb7cb265b5e88c8de1acddb
ACM-Anik <anikmojumder5252@gmail.com>
2023-11-04 23:59:57 +0600

    throttles function added

--- script.js
+++ script.js
@@ -8,4 +8,38 @@ document.addEventListener("click", e => {
     if (handle != null) {
         onHandleClick(handle);
     }
-})
+});
+
+const throttleProgressBar = throttle(() => {
+    document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
+}, 250);
+
+window.addEventListener("resize", throttleProgressBar);
+document.querySelectorAll(".progress-bar").forEach(calculateProgressBar);
+
+
+
+function throttle(cb, delay = 1000) {
+    let shouldWait = false;
+    let waitingArgs;
+    const timeoutFunc = () => {
+        if (waitingArgs == null) {
+            shouldWait = false;
+        } else {
+            cb(...waitingArgs);
+            waitingArgs = null;
+            setTimeout(timeoutFunc, delay);
+        }
+    }
+
+    return (...args) => {
+        if (shouldWait) {
+            waitingArgs = args;
+            return;
+        }
+
+        cb(...args);
+        shouldWait = true;
+        setTimeout(timeoutFunc, delay);
+    }
+};
