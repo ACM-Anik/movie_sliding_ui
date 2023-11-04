99f11a3e8121e5bf188c4e5a40d2ba39590f1add
ACM-Anik <anikmojumder5252@gmail.com>
2023-11-05 00:03:48 +0600

    calculateProgressBar added

--- script.js
+++ script.js
@@ -41,41 +41,7 @@ function calculateProgressBar(progressBar) {
     }
 };
 
-function onHandleClick(handle) {
-    const progressBar = handle.closest(".row").querySelector(".progress-bar");
-    const slider = handle.closest(".container").querySelector(".slider");
 
-    const sliderIndex = parseInt(
-        getComputedStyle(slider).getPropertyValue("--slider-index")
-    )
-
-    const progressBarItemCount = progressBar.children.length;
-    console.log(progressBarItemCount);
-
-    if (handle.classList.contains("left-handle")) {
-        if (sliderIndex - 1 < 0) {
-            slider.style.setProperty("--slider-index", progressBarItemCount - 1);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[progressBarItemCount - 1].classList.add("active");
-        } else {
-            slider.style.setProperty("--slider-index", sliderIndex - 1);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[sliderIndex - 1].classList.add("active");
-        }
-    }
-
-    if (handle.classList.contains("right-handle")) {
-        if (sliderIndex + 1 >= progressBarItemCount) {
-            slider.style.setProperty("--slider-index", 0);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[0].classList.add("active");
-        } else {
-            slider.style.setProperty("--slider-index", sliderIndex + 1);
-            progressBar.children[sliderIndex].classList.remove("active");
-            progressBar.children[sliderIndex + 1].classList.add("active");
-        }
-    }
-};
 
 function throttle(cb, delay = 1000) {
     let shouldWait = false;
